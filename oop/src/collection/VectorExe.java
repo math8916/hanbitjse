a9cb4c82252d515fd0b041bc8db993e23e0f9a44
math8916 <math89@gmail.com>
2016-06-23 17:50:26 +0900

    fff

--- oop/src/collection/VectorExe.java
+++ oop/src/collection/VectorExe.java
@@ -6,25 +6,33 @@ package collection;
 import java.util.List;
 import java.util.Vector;
 
+import school.StudentBean;
+
 /**
- * @date   : 2016. 6. 23.
+ * @date : 2016. 6. 23.
  * @author : 최인철
  * @file_name : ListExe.java
- * @story : 
-*/
+ * @story :
+ */
 public class VectorExe {
-public static void main(String[] args) {
-	List<String> v= new Vector<String>(2,5);
-	System.out.println("    Vector ===========");
-	System.out.println("size"+v.size());
-	
-	v.add("박지성");
-	v.add("박  성");
-	v.add("박 1 성");
-	v.add("박 2 성");
-	
-	System.out.println("    요소 추가후 ===========");
-	System.out.println("size"+v.size());
+	public static void main(String[] args) {
+		List<StudentBean> v = new Vector<StudentBean>();
 
-}
+		StudentBean hong = new StudentBean("hong", "1", "홍길동", "700814-1");
+		StudentBean kim = new StudentBean("kim", "1", "김길동", "750814-1");
+		StudentBean lee = new StudentBean("lee", "1", "lee길동", "705814-1");
+		System.out.println("    Vector ===========");
+		System.out.println("size" + v.size());
+
+		v.add(hong);
+		v.add(kim);
+		v.add(lee);
+
+		System.out.println("    요소 추가후 ===========");
+		System.out.println("size" + v.size());
+//		System.out.println(v.toString());
+		for (int i = 0; i < v.size(); i++) {
+			System.out.println(v.get(i).getName());
+		}
+	}
 }
